6e69629488cfb489021ea51e66fd08147d991aa6
Julia <julia-15-07@mail.ru>
2022-07-05 23:26:14 +0300

    Homework03 added

---
+++ Homeworks/Homework03/Program.cs
@@ -0,0 +1,42 @@
+﻿// принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Palindrom (int num)
+{
+    int a1 = num / 10000;
+    int a2 = (num / 1000) % 10;
+    int a4 = (num % 100) / 10;
+    int a5 = num % 10;
+
+    if (a1 == a5)
+    {
+        if (a2 == a4)
+        Console.WriteLine("Yes, it is palindrom");
+    }
+    else Console.WriteLine("No, it is not palindrom");
+}
+
+Console.WriteLine("Input five-digit number: "); 
+int num = Convert.ToInt32(Console.ReadLine());
+
+Palindrom (num);
+
+// принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+/*void CubeTabl(int n)
+{
+    int current = 1;
+    
+    while (current <= n)
+    {
+        int cube = current*current*current;
+        Console.Write(cube + " "); 
+        current ++;
+    }
+
+}
+
+Console.WriteLine("Input a number "); 
+int n = Convert.ToInt32(Console.ReadLine());
+    
+CubeTabl(n);
+*/
