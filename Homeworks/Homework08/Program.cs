252140e74f07e83845d2c741d2f4f89a9d7a3a4c
Julia <julia-15-07@mail.ru>
2022-07-31 20:20:53 +0300

    Homework08 added

---
+++ Homeworks/Homework08/Program.cs
@@ -0,0 +1,88 @@
+﻿int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++) // по строкам
+        for(int j = 0; j < columns; j++) // по всем элементам строки
+            newArray[i,j] = new Random().Next(minValue, maxValue + 1);
+    
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++) // количество строк (0)
+    {
+        for(int j = 0; j < array.GetLength(1); j++) // количество строк (1)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+}
+
+int[,] DecreaseArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+  return array;
+}
+ 
+int SumInRows(int[,] array, int i)
+{
+        int sum = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sum +=array [i,j];
+             
+        }
+    return sum;    
+} 
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m,n,min,max);
+
+// Задача 54:Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Show2dArray(myArray);
+
+//Console.WriteLine("New massive:");
+//Show2dArray(DecreaseArray(myArray));
+
+// Задача 56:Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int minSumRow = 0;
+int sumRow1 = SumInRows(myArray, 0);
+for(int i = 1; i < myArray.GetLength(0); i++)
+{
+    int sumRow = SumInRows(myArray, i);
+    if (sumRow < sumRow1)
+    {
+        sumRow1 = sumRow;
+        minSumRow = i;
+    }
+
+}
+
+Console.WriteLine($"Min sum elements {sumRow1} is in rows {minSumRow+1}");
+
---
+++ Homeworks/Homework09/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+
+int CountElements(int n)
+{
+    
+    if(n >= 10)
+    {
+        return 1 + CountElements(n / 10);
+    }
+    return n;
+}
+
+Console.WriteLine(CountElements(123));
+// 12345 -> 5
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+/*
+int SumNumber(int m, int n)
+{
+    
+    if(m < n)
+    {
+        return m + SumNumber(m + 1, n);
+    }
+   
+   return m;
+}
+
+Console.WriteLine(SumNumber(0,10));
+*/
